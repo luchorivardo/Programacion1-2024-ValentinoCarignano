3f70df1c760413dac5c155c452449d7994ea8685
Valentino Carignano <valentino.carignano1@gmail.com>
2024-04-23 17:03:27 -0300

    ejerciciosWHILE

--- Estructuras Repetitivas/ejerciciosWHILE/ejerciciosWHILE/Program.cs
+++ Estructuras Repetitivas/ejerciciosWHILE/ejerciciosWHILE/Program.cs
@@ -63,6 +63,65 @@ namespace ejerciciosWHILE
             Console.WriteLine($"La cantidad de nombres ingresados es: {cantidadNombres}");*/
 
             //Ejercicio 4.
+            /*Console.WriteLine("Ingrese los datos requeridos para calcular el total de una factura: ");
+            Console.WriteLine("(Cuando el costo unitario ingresado sea 0 el programa finalizara)");
+            double costoUnitario = 1;
+            int cantidadComprada = 0;
+            double totalAdeudado = 0;
+
+            while (costoUnitario != 0)
+            {
+                Console.Write("Costo unitario: ");
+                if (!double.TryParse(Console.ReadLine()?.Replace(".",","), out costoUnitario) || costoUnitario < 0)
+                {
+                    Console.WriteLine("Error, numero no valido. Ingrese otro.");
+                    costoUnitario = 1;
+                }
+                else
+                {
+                    Console.Write("Cantidad comprada: ");
+                    if (!Int32.TryParse(Console.ReadLine(), out cantidadComprada) || cantidadComprada < 0)
+                    {
+                        Console.WriteLine("Error, numero no valido. Ingrese otro.");
+                        costoUnitario = 1;
+                    }
+                    else
+                    {
+                        totalAdeudado += costoUnitario * cantidadComprada;
+                    }
+                }
+            }
+            Console.WriteLine($"El total adeudado es de: ${totalAdeudado}");*/
+
+            //Ejercicio 5.
+            /*Console.WriteLine("Ingrese los datos requeridos para calcular el consumo total del cliente: ");
+            double costoUnitario = 0;
+            int cantidadProductos = 0;
+            double totalAdeudado = 0;
+            int i = 1;
+
+            Console.Write("Cantidad comprada: ");
+            if (!Int32.TryParse(Console.ReadLine(), out cantidadProductos) || cantidadProductos <= 0)
+            {
+                Console.WriteLine("Error, numero no valido. Ingrese otro.");
+            }
+            else
+            {
+                while (i <= cantidadProductos)
+                {
+                    Console.Write($"Precio producto {i}: ");
+                    if (!double.TryParse(Console.ReadLine()?.Replace(".", ","), out costoUnitario) || costoUnitario < 0)
+                    {
+                        Console.WriteLine("Error, numero no valido. Ingrese otro.");
+                    }
+                    else
+                    {
+                        totalAdeudado += costoUnitario;
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine($"El total adeudado por el cliente es de: ${totalAdeudado}");*/
         }
     }
 }
