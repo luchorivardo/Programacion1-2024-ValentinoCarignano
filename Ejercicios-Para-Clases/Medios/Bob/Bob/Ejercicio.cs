a2181b5ce753fda99d5ffcbf5b2f0506fdf86801
Valentino Carignano <valentino.carignano1@gmail.com>
2024-08-28 23:08:07 -0300

    bobtest

--- Ejercicios-Para-Clases/Medios/Bob/Bob/Ejercicio.cs
+++ Ejercicios-Para-Clases/Medios/Bob/Bob/Ejercicio.cs
@@ -11,7 +11,7 @@
 
             Bob sólo responde una de cinco cosas:
 
-            "Sure". Esta es su respuesta si le haces una pregunta, como "How are you?". La convención que se utiliza para las preguntas es que terminan con un signo de interrogación.
+            "Sure." Esta es su respuesta si le haces una pregunta, como "How are you?". La convención que se utiliza para las preguntas es que terminan con un signo de interrogación.
             "Whoa, chill out!". Esta es su respuesta si LE GRITAS. La convención que se usa para gritar es TODAS LAS LETRAS EN MAYÚSCULAS.
             "Calm down, I know what I'm doing!". Esto es lo que dice si le gritas una pregunta.
             "Fine. Be that way!". Así es como responde al silencio. La convención que se utiliza para el silencio es nada o varias combinaciones de caracteres en blanco.
@@ -22,7 +22,48 @@
     {
         public static string Response(string statement)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            string response = "Whatever.";
+
+            if (!string.IsNullOrWhiteSpace(statement))
+            {
+                if (statement.Contains('?'))
+                {
+                    int posInterrogante = statement.Trim().Length - 1;
+                    int index = statement.LastIndexOf('?');
+                    
+                    if (index == posInterrogante)
+                    {
+                        response = "Sure.";
+                    }
+                }
+                else if (statement.ToUpper() == statement)
+                {
+                    foreach(char c in statement)
+                    {
+                        if (char.IsLetter(c))
+                        {
+                            response = "Whoa, chill out!";
+                        }
+                    }
+                }
+                
+                if(statement.ToUpper() == statement && statement.Contains("?"))
+                {
+                    foreach (char c in statement)
+                    {
+                        if (char.IsLetter(c))
+                        {
+                            response = "Calm down, I know what I'm doing!";
+                        }
+                    }
+                }        
+            }
+            else
+            {
+                response = "Fine. Be that way!";
+            }
+
+            return response;
         }
     }
 }
